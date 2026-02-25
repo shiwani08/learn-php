21f272f39328592f54cfe46bc5962f3c400e8d41
shiwani08 <shiwanisoni29082002@gmail.com>
2026-02-25 22:52:34 +0530

    data types

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 
 <body>
   Hello <?php echo $user; ?>!
+  <?php
+  $foo = 10;   // $foo is an integer
+  $bar = (bool) $foo;   // $bar is a boolean
+  echo $bar;
+  echo gettype($bar);
+  echo $foo;
+  print $bar;
+  ?>
 </body>
 
 </html>
